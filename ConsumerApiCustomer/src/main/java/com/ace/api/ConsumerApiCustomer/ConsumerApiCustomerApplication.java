bcd9cde2aba6fb4e99495c1b3e94a00eef22ea2d
axelcorreae <axellink4@gmail.com>
2024-03-19 23:28:32 -0600

    Primera carga

---
+++ ConsumerApiCustomer/src/main/java/com/ace/api/ConsumerApiCustomer/ConsumerApiCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.ace.api.ConsumerApiCustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApiCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApiCustomerApplication.class, args);
+	}
+
+}
---
+++ ConsumerApiCustomer/src/test/java/com/ace/api/ConsumerApiCustomer/ConsumerApiCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ace.api.ConsumerApiCustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApiCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
